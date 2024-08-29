6a00fceb720808f91df4c86fd7c64e4955487d83
alvaroherbon <alvaroherbon93@gmail.com>
2024-08-29 10:47:04 +0200

    added search component

--- linuxWallpaperChanger/src/main.py
+++ linuxWallpaperChanger/src/main.py
@@ -23,7 +23,7 @@ import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
-from gi.repository import Gtk, Gio, Adw
+from gi.repository import Adw, Gio, Gtk, GLib
 from .window import LinuxwallpaperchangerWindow
 
 
@@ -33,9 +33,28 @@ class LinuxwallpaperchangerApplication(Adw.Application):
     def __init__(self):
         super().__init__(application_id='org.gnome.linuxWallpaperChanger',
                          flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+
+        self.settings = Gio.Settings(schema_id="org.gnome.linuxWallpaperChanger")
+
+
+        dark_mode = self.settings.get_boolean("dark-mode")
+        style_manager = Adw.StyleManager.get_default()
+        if dark_mode:
+            style_manager.set_color_scheme(Adw.ColorScheme.FORCE_DARK)
+        else:
+            style_manager.set_color_scheme(Adw.ColorScheme.DEFAULT)
+
+        dark_mode_action = Gio.SimpleAction(name="dark-mode",
+                                            state=GLib.Variant.new_boolean(dark_mode))
+
         self.create_action('quit', lambda *_: self.quit(), ['<primary>q'])
         self.create_action('about', self.on_about_action)
         self.create_action('preferences', self.on_preferences_action)
+        self.set_accels_for_action('win.open', ['<Ctrl>o'])
+        dark_mode_action = Gio.SimpleAction(name="dark-mode",state=GLib.Variant.new_boolean(False))
+        dark_mode_action.connect("activate", self.toggle_dark_mode)
+        dark_mode_action.connect("change-state", self.change_color_scheme)
+        self.add_action(dark_mode_action)
 
     def do_activate(self):
         """Called when the application is activated.
@@ -78,8 +97,28 @@ class LinuxwallpaperchangerApplication(Adw.Application):
         if shortcuts:
             self.set_accels_for_action(f"app.{name}", shortcuts)
 
+    def toggle_dark_mode(self, action, _):
+        state = action.get_state()
+        old_state = state.get_boolean()
+        new_state = not old_state
+        action.change_state(GLib.Variant.new_boolean(new_state))
+
+
+    def change_color_scheme(self, action, new_state):
+        dark_mode = new_state.get_boolean()
+        style_manager = Adw.StyleManager.get_default()
+        if dark_mode:
+            style_manager.set_color_scheme(Adw.ColorScheme.FORCE_DARK)
+        else:
+            style_manager.set_color_scheme(Adw.ColorScheme.DEFAULT)
+        action.set_state(new_state)
+        self.settings.set_boolean("dark-mode", dark_mode)
+
 
 def main(version):
     """The application's entry point."""
     app = LinuxwallpaperchangerApplication()
     return app.run(sys.argv)
+
+
+
--- linuxWallpaperChanger/src/window.py
+++ linuxWallpaperChanger/src/window.py
@@ -17,14 +17,58 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw
-from gi.repository import Gtk
+from gi.repository import Adw, Gio, Gtk, GLib
 
 @Gtk.Template(resource_path='/org/gnome/linuxWallpaperChanger/window.ui')
 class LinuxwallpaperchangerWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'LinuxwallpaperchangerWindow'
 
-    label = Gtk.Template.Child()
+    main_text_view = Gtk.Template.Child()
+    search_entry = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        save_action = Gio.SimpleAction(name="save-as")
+        save_action.connect("activate", self.save_file_dialog)
+        self.add_action(save_action)
+
+
+    def save_file_dialog(self, action, _):
+        native = Gtk.FileDialog()
+        native.save(self, None, self.on_save_response)
+
+    def on_save_response(self, dialog, result):
+        file = dialog.save_finish(result)
+        if file is not None:
+            self.save_file(file)
+
+    def save_file(self, file):
+        buffer = self.main_text_view.get_buffer()
+
+    # Retrieve the iterator at the start of the buffer
+        start = buffer.get_start_iter()
+    # Retrieve the iterator at the end of the buffer
+        end = buffer.get_end_iter()
+    # Retrieve all the visible text between the two bounds
+        text = buffer.get_text(start, end, False)
+
+    # If there is nothing to save, return early
+        if not text:
+            return
+
+        bytes = GLib.Bytes.new(text.encode('utf-8'))
+
+    # Start the asynchronous operation to save the data into the file
+        file.replace_contents_bytes_async(bytes,None,False,Gio.FileCreateFlags.NONE,None,self.save_file_complete)
+
+    def save_file_complete(self, file, result):
+        res = file.replace_contents_finish(result)
+        info = file.query_info("standard::display-name",Gio.FileQueryInfoFlags.NONE)
+        if info:
+            display_name = info.get_attribute_string("standard::display-name")
+        else:
+            display_name = file.get_basename()
+        if not res:
+            print(f"Unable to save {display_name}")
+
